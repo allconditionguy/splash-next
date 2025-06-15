f674ad50cedd8fe46ca9d814443adb1651d55e31
allconditionguy <asdesouza@st.knust.edu.gh>
2025-06-15 22:42:18 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground flex justify-center">Wayoe Entrance</div>
+    <div className="bg-background text-foreground flex justify-center">just tryouts</div>
   )
 }
