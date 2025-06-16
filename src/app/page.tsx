dba5c9042c7c03a6c9d07d630c49d89fba692ea8
allconditionguy <asdesouza@st.knust.edu.gh>
2025-06-16 23:26:28 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground flex justify-center">just tryouts</div>
+    <div className="bg-background text-foreground flex justify-center">move boys</div>
   )
 }
