6b2797f5136d6db79197f4365be3f78ca544fde6
allconditionguy <asdesouza@st.knust.edu.gh>
2025-06-22 23:00:09 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground flex justify-center">action dey for here</div>
+    <div className="bg-background text-foreground flex justify-center">starting off with next</div>
   )
 }
