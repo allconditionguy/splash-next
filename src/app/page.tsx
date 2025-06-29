464936cf38fc35ab93a07909e1e3aefa672221f5
allconditionguy <asdesouza@st.knust.edu.gh>
2025-06-29 23:00:59 +0000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div className="bg-background text-foreground flex justify-center">introdcution</div>
+    <div className="bg-background text-foreground flex justify-center"></div>
   )
 }
